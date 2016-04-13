5ad6c1bea434933319b01a69bacbe045fd097ed6
jeremiegirault <jeremie.girault@gmail.com>
2016-04-13 11:23:48 +0200

    removed code for before

--- cocoaheads-playgrounds.playground/Pages/UIPrototyping.xcplaygroundpage/Contents.swift
+++ cocoaheads-playgrounds.playground/Pages/UIPrototyping.xcplaygroundpage/Contents.swift
@@ -32,69 +32,7 @@ class CircleButtonListView: UIView {
     }
     
     private func setup() {
-        putItems(self.items)
-    }
-    
-    override func layoutSubviews() {
-        subviews.enumerate().forEach { index, view in
-            view.center = CGPoint(x: computeX(index, viewCount: subviews.count), y: center.y)
-        }
-    }
-    
-    private func computeX(viewIndex: Int, viewCount: Int) -> CGFloat {
-        return CGFloat(viewIndex+1) * bounds.width / CGFloat(viewCount+1)
-    }
-    
-    private func putItems(items: [CircleButtonListViewItem], animatedFromIndex: Int? = nil) {
-        subviews.forEach { $0.removeFromSuperview() }
-        
-        items
-            .prefix(maxItemCount)
-            .enumerate()
-            .forEach { index, item in
-                addItem(item, atIndex: index)
-            }
-        
-        if let animatedFromIndex = animatedFromIndex {
-            let x = computeX(animatedFromIndex, viewCount: min(maxItemCount, items.count))
-            
-            subviews.forEach { view in
-                view.center = CGPoint(x: x, y: center.y)
-                view.alpha = 0.0
-            }
-            
-            UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 0.3, options: [], animations: {
-                self.subviews.forEach { $0.alpha = 1.0 }
-                self.setNeedsLayout()
-                self.layoutIfNeeded()
-                }, completion: nil)
-        }
-    }
-    
-    private func addItem(item: CircleButtonListViewItem, atIndex index: Int) {
-        let rv = UIButton(type: .Custom)
-        
-        let size: CGFloat = 50
-        rv.addTarget(self, action: #selector(CircleButtonListView.buttonTouched(_:)), forControlEvents: .TouchUpInside)
-        rv.tag = index
-        rv.setImage(item.image, forState: .Normal)
-        rv.backgroundColor = item.color
-        rv.layer.cornerRadius = size*0.5
-        rv.bounds = CGRect(x: 0, y: 0, width: size, height: size)
-        
-        addSubview(rv)
-    }
-    
-    func buttonTouched(button: UIButton) {
-        putItems(self.items, animatedFromIndex: button.tag)
     }
 }
-
-XCPlaygroundPage.currentPage.liveView = CircleButtonListView(items: [
-    CircleButtonListViewItem(title: "test", image: image, color: .orangeColor()),
-    CircleButtonListViewItem(title: "test", image: image, color: .yellowColor()),
-    CircleButtonListViewItem(title: "test", image: image, color: .purpleColor()),
-    CircleButtonListViewItem(title: "test", image: image, color: .blueColor())
-    ])
 
 //: [Next](@next)
