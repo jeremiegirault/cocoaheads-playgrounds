e2d01ab6fbeaea92a7bfc2aa04a60b72cbe9c0ab
jeremiegirault <jeremie.girault@gmail.com>
2016-04-14 11:21:54 +0200

    updated before

--- cocoaheads-playgrounds.playground/Pages/UIPrototyping.xcplaygroundpage/Contents.swift
+++ cocoaheads-playgrounds.playground/Pages/UIPrototyping.xcplaygroundpage/Contents.swift
@@ -5,34 +5,5 @@
 import UIKit
 import XCPlayground
 
-struct CircleButtonListViewItem {
-    let title: String
-    let image: UIImage?
-    let color: UIColor
-}
-
-let image = [#Image(imageLiteral: "Icon100.png")#]
-
-class CircleButtonListView: UIView {
-    let items: [CircleButtonListViewItem]
-    let maxItemCount = 4
-    
-    init(items: [CircleButtonListViewItem]) {
-        self.items = items
-        super.init(frame: CGRect(x: 0, y: 0, width: 320, height: 100))
-        
-        setup()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        self.items = []
-        super.init(coder: aDecoder)
-        
-        setup()
-    }
-    
-    private func setup() {
-    }
-}
 
 //: [Next](@next)
